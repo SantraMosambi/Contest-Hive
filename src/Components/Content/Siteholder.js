a2de5be3affa9149fbb66a1060fb7d7199e62e50
unknown <shubhamkarb18@gmail.con>
2023-01-30 19:22:30 +0530

    Legacy Code

--- src/Components/Content/Siteholder.js
+++ src/Components/Content/Siteholder.js
@@ -1,47 +1,47 @@
 import React from "react";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
-import { useEffect } from "react";
-import { useState } from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
 
 const Siteholder = () => {
-  const APII = "https://kontests.net/api/v1/sites";
+  // const APII = "https://kontests.net/api/v1/sites";
 
-  const fetchApiData = async (url) => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const fetchApiData = async (url) => {
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchApiData(APII);
-  }, []);
+  // useEffect(() => {
+  //   fetchApiData(APII);
+  // }, []);
 
   const datas = [
     { id: 1, name: "All" },
     { id: 2, name: "CodeChef" },
     { id: 3, name: "CodeForces" },
     { id: 4, name: "AtCoder" },
-    { id: 5, name: "TopCoder" },
-    { id: 6, name: "HackerRank" },
-    { id: 7, name: "HackerEarth" },
-    { id: 8, name: "LeetCode" },
-    { id: 9, name: "Kick Start" },
-    { id: 10, name: "Google Code Jam" },
+    { id: 5, name: "HackerRank" },
+    { id: 6, name: "HackerEarth" },
+    { id: 7, name: "LeetCode" },
+    { id: 8, name: "Kick Start" },
   ];
 
   return (
     <div className="contentholder">
       <div className="main-container">
-        <Stack direction="row" spacing={3}>
-          {datas.map((item) => (
-            <Chip label={item.name} />
-          ))}
-        </Stack>
+        <div className="box">
+          <Stack direction="row" spacing={3}>
+            {datas.map((item, id) => (
+              <Chip label={item.name} key={id}  />
+            ))}
+          </Stack>
+        </div>
       </div>
     </div>
   );
