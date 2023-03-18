b2d85f30ef340accb8c410695cdfbe9e10620a89
unknown <shubhamkarb18@gmail.con>
2023-03-18 15:05:47 +0530

    Time

--- src/Components/Content/table.js
+++ src/Components/Content/table.js
@@ -7,7 +7,7 @@ import Link from "@mui/material/Link";
 const MainTable = () => {
   const [data, setData] = useState([]);
 
-  const API = "https://kontests.net/api/v1/codeforces";
+  const API = "https://kontests.net/api/v1/all";
 
   const fetchApiData = async (url) => {
     try {
@@ -23,13 +23,27 @@ const MainTable = () => {
     fetchApiData(API);
   }, []);
 
-  const rows = data.map((row) => ({
-    name: row.name,
-    sTime: new Date(row.start_time).toLocaleString(),
-    eTime: new Date(row.end_time).toLocaleString(),
-    duration: new Date(row.duration).getHours(),
-    site: row.url,
-  }));
+  const rows = data.map((row) => {
+    const startTime = new Date(row.start_time);
+    const endTime = new Date(row.end_time);
+    const durationInSeconds = (endTime - startTime) / 1000;
+    const durationInDays = Math.floor(durationInSeconds / (3600 * 24));
+    const durationInHours = Math.floor((durationInSeconds % (3600 * 24)) / 3600);
+    const durationInMinutes = Math.floor((durationInSeconds % 3600) / 60);
+    const duration = durationInDays > 0 ? `${durationInDays} days and ${durationInHours.toString().padStart(2, '0')}:${durationInMinutes.toString().padStart(2, '0')} Hr` : `${durationInHours.toString().padStart(2, '0')}:${durationInMinutes.toString().padStart(2, '0')} Hr`;
+  
+    return {
+      name: row.name,
+      siteName: row.site,
+      sTime: startTime.toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' }) + ' ' + startTime.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' }),
+      eTime: endTime.toLocaleDateString('en-GB', { day: 'numeric', month: 'short', year: 'numeric' }) + ' ' + endTime.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' }),
+      duration: duration,
+      site: row.url,
+    };
+  });
+
+  
+  
 
   const columns = [
     // { field: "id", headerName: "ID", width: 70},
@@ -49,20 +63,20 @@ const MainTable = () => {
         );
       },
     },
-    { field: "sTime", headerName: "Start Time", width: 230 },
-    { field: "eTime", headerName: "End Time", width: 230 },
-    { field: "duration", headerName: "Duration", width: 130 },
+    { field: "sTime", headerName: "Start Time", width: 200 },
+    { field: "eTime", headerName: "End Time", width: 200 },
+    { field: "duration", headerName: "Duration", width: 190 },
     { field: "siteName", headerName: "Site Name", width: 130 },
   ];
 
   return (
     <div className="contentholdr">
       <div className="main-container">
-        <div style={{ height: 400, width: "100%" }}>
+        <div style={{ height: 700, width: "100%" }}>
           <DataGrid
             rows={rows}
             columns={columns}
-            pageSize={5}
+            pageSize={11}
             rowsPerPageOptions={[5]}
             getRowId={() => generateRandom()}
           />
